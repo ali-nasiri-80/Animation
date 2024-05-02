77db9efb8852d79f8956d95b5241a0ad968ac93d
ali-nasiri-80 <alinasiri576@gmail.com>
2024-05-02 12:27:16 +0330

    Combina Animation with AnimationSet

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,16 +18,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View playButton=findViewById(R.id.fab_main);
+        TranslateAnimation translateAnimation=new TranslateAnimation(Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_PARENT,-1,Animation.ABSOLUTE,0,Animation.ABSOLUTE,0);
+
         final ImageView imageView=findViewById(R.id.iv_main);
 
         RotateAnimation rotateAnimation=new RotateAnimation(0,360,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-        rotateAnimation.setDuration(1000);
-
+        AlphaAnimation alphaAnimation=new AlphaAnimation(1,0);
+        AnimationSet animationSet=new AnimationSet(true);
+        animationSet.setDuration(1000);
+        animationSet.setInterpolator(new AccelerateInterpolator());
+        animationSet.setFillAfter(true);
+        animationSet.addAnimation(rotateAnimation);
+        animationSet.addAnimation(alphaAnimation);
+        animationSet.addAnimation(translateAnimation);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            imageView.startAnimation(rotateAnimation);
+            imageView.startAnimation(animationSet);
 
             }
         });
