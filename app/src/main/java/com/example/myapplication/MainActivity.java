43b8504308426ce9d8c86b55772052e8bf09bca1
ali-nasiri-80 <alinasiri576@gmail.com>
2024-05-02 11:36:42 +0330

    TranslateAnimation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,10 +2,9 @@ package com.example.myapplication;
 
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
+import android.view.animation.*;
 import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,19 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ScaleAnimation scaleAnimation=new ScaleAnimation(1,2,1,2,Animation.RELATIVE_TO_SELF,.5f,Animation.RELATIVE_TO_SELF,.5f);
-        scaleAnimation.setDuration(1000);
-        scaleAnimation.setFillAfter(true);
-        scaleAnimation.setRepeatCount(Animation.INFINITE);
-        scaleAnimation.setRepeatMode(Animation.REVERSE);
+        TranslateAnimation translateAnimation=new TranslateAnimation(Animation.RELATIVE_TO_PARENT,0,Animation.RELATIVE_TO_PARENT,0,Animation.RELATIVE_TO_PARENT,0,Animation.RELATIVE_TO_PARENT,.7f);
+        translateAnimation.setDuration(1000);
+        translateAnimation.setFillAfter(true);
+        translateAnimation.setInterpolator(new BounceInterpolator());
 
        final  ImageView imageView=findViewById(R.id.iv_main);
         View playButton=findViewById(R.id.fab_main);
-        scaleAnimation.setDuration(1000);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"imageView clicked!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageView.startAnimation(scaleAnimation);
+                imageView.startAnimation(translateAnimation);
 
             }
         });
